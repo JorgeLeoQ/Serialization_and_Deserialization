792674fb002af7542b94705ea350790370f2b9f6
Leo <jleonardoqv@gmail.com>
2021-11-29 22:55:40 +0100

    aggiornato Deserializzazione_2

--- Deserializzazione_2/Deserializzazione_2/Program.cs
+++ Deserializzazione_2/Deserializzazione_2/Program.cs
@@ -14,12 +14,14 @@ namespace Deserializzazione_2
 
             //Deserializzazione 
             XmlSerializer xmls = new XmlSerializer(typeof(AddressDetails));
-            using(TextReader tr = new StreamReader(@"C:\output.xml"))
+            using(TextReader tr = new StreamReader(@"..\output.xml"))
             {
                 details = (AddressDetails)xmls.Deserialize(tr);
             }
 
-            Console.WriteLine(details.City.ToString());
+            Console.WriteLine("StreetName: " + details.StreetName);
+            Console.WriteLine("City: " + details.City);
+            Console.WriteLine("HouseNo: " + details.HouseNo);
         }
     }
 }
